033e893209f4efc54e20cdc7ff20fdf5b33a98a8
willian <br.techlabz@proton.me>
2026-05-07 10:16:46 -0300

    Obtendo um usuario por id e tratando o erro de não encontrado

--- src/main/java/br/techlabz/workshopmongo/services/UserService.java
+++ src/main/java/br/techlabz/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package br.techlabz.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.techlabz.workshopmongo.domain.User;
 import br.techlabz.workshopmongo.repositories.UserRepository;
+import br.techlabz.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
 	}
 
 }
