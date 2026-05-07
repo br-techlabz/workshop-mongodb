df3f6097cbc4f6c5b160c29c1d9d92b6875a5c09
willian <br.techlabz@proton.me>
2026-05-07 08:55:25 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/br/techlabz/workshopmongo/config/Instantiation.java
@@ -0,0 +1,34 @@
+package br.techlabz.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import br.techlabz.workshopmongo.domain.User;
+import br.techlabz.workshopmongo.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	
+	@Autowired
+	private UserRepository repo;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		repo.deleteAll();
+				
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob =  new User(null, "Bob Grey", "bob@gmail.com"); 
+		
+		repo.saveAll(Arrays.asList(maria, alex, bob));
+		
+
+		
+	}
+
+}
