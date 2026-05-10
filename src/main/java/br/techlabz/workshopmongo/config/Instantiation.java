f6fd85f5fb4fe3f47a899d6f815e2de580a7cb52
willian <br.techlabz@proton.me>
2026-05-10 12:16:26 -0300

    referenciando post do usuário

--- src/main/java/br/techlabz/workshopmongo/config/Instantiation.java
+++ src/main/java/br/techlabz/workshopmongo/config/Instantiation.java
@@ -1,12 +1,17 @@
 package br.techlabz.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import br.techlabz.workshopmongo.domain.Post;
 import br.techlabz.workshopmongo.domain.User;
+import br.techlabz.workshopmongo.dto.AuthorDTO;
+import br.techlabz.workshopmongo.repositories.PostRepository;
 import br.techlabz.workshopmongo.repositories.UserRepository;
 
 @Configuration
@@ -14,18 +19,38 @@ public class Instantiation implements CommandLineRunner {
 	
 	
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepo;
+	
+	@Autowired
+	private PostRepository postRepo;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
-		repo.deleteAll();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		
+		postRepo.deleteAll();
+		userRepo.deleteAll();
+		
 				
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob =  new User(null, "Bob Grey", "bob@gmail.com"); 
 		
-		repo.saveAll(Arrays.asList(maria, alex, bob));
+		userRepo.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu Viagem","Vou ciajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje!", new AuthorDTO(maria));
+				
+		postRepo.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPost().addAll(Arrays.asList(post1, post2));
+		userRepo.save(maria);
+		
+		
 		
 
 		
---
+++ src/main/java/br/techlabz/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package br.techlabz.workshopmongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import br.techlabz.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
